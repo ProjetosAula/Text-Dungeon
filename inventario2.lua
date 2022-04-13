c7eb95e7697caaa4ec09600699d0598b8dbd0981
Seikm <luisfrancisco467@gmail.com>
2022-04-12 21:30:26 -0300

    Fazendo algumas organizações

--- inventario2.lua
+++ inventario2.lua
@@ -1,7 +1,9 @@
-local Inventario = require("Modules/Inventario")
 local JSON = require("json")
+local Inventario = require("Modules/Inventario")
+
 local PlayerArquivo = io.open("Data/Player.json", "r")
 local Player = JSON.decode(PlayerArquivo:read("*all"))
+
 print(Inventario.Mostrar(Player))
 print(Inventario.Remover(Player, "pica de cavalo"))
 print(Inventario.Remover(Player, "pica de urubu"))
